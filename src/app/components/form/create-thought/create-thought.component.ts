5306a3824ded727d2616e534ef31f96f60395224
Lucas Ferrari <lucasfcorrea2206@gmail.com>
2024-08-02 10:39:09 -0300

    part 2 finished

---
+++ src/app/components/form/create-thought/create-thought.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-thought',
+  templateUrl: './create-thought.component.html',
+  styleUrls: ['./create-thought.component.scss']
+})
+export class CreateThoughtComponent implements OnInit {
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: 'modelo1'
+  }
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  createThought() {
+    alert("Novo pensamento criado!")
+  }
+
+  cancel() {
+    alert("Novo pensamento cancelado!")
+  }
+}
